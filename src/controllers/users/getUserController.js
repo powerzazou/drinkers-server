75e77065aa8c6c9d5ee72905c5ebe965f694e1d0
Vincent Dumont <vincent.e.dumont@gmail.com>
2018-04-28 20:23:29 +0200

    bugfix on get users/:id

--- src/controllers/users/getUserController.js
+++ src/controllers/users/getUserController.js
@@ -3,7 +3,14 @@ import User from '../../models/user'
 
 export default async function getUserController (req, res) {
   try {
-    const user = await User.getUser(req.params.id)
+    let user = await User.getUser(req.params.id)
+    const beers = await Promise.all([
+      fetch(`https://api.punkapi.com/v2/beers/${user.beer1}`).then((response) => response.json()),
+      fetch(`https://api.punkapi.com/v2/beers/${user.beer2}`).then((response) => response.json())
+    ])
+    user = user.toObject()
+    user.beer1 = beers[0][0]
+    user.beer2 = beers[1][0]
     res.send(user)
   } catch (error) {
     // Dans la vraie vie il faudrait un vrai systeme de log
