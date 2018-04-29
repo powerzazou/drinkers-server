aaaac8e0bebb22d56646a276a9c823762966c469
Vincent Dumont <vincent.e.dumont@gmail.com>
2018-04-29 11:19:10 +0200

    add CORS middleware

--- src/app.js
+++ src/app.js
@@ -18,6 +18,15 @@ if (app.get('env') !== 'test') {
   app.use(createLogger(app.get('env') === 'development' ? 'dev' : 'combined'))
 }
 
+// Allow CORS
+const allowCrossDomainMiddleware = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS,PATCH')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+  next()
+}
+app.use(allowCrossDomainMiddleware)
+
 // Middlewares et routes applicatifs
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(Users)
